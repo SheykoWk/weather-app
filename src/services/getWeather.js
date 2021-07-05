91d633e161a2ee990512eb8953c2f190d2fbfeeb
sheyko <sheyko.wk@gmail.com>
2021-07-04 23:47:05 -0500

    solution to the api request https from the weather api

--- src/services/getWeather.js
+++ src/services/getWeather.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const getWeather =  (ubi) =>{
-    const URL = `http://api.weatherapi.com/v1/current.json?key=bf5ce92320604a50b1312030210207&q=${ubi}&aqi=no`;
+    const URL = `https://api.weatherapi.com/v1/current.json?key=bf5ce92320604a50b1312030210207&q=${ubi}&aqi=no`;
     const res = axios(URL)
     return res
 }
