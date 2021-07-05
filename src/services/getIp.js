1752939dc49d35e6307033b9b55d3d61c44babf3
sheyko <sheyko.wk@gmail.com>
2021-07-04 23:43:34 -0500

    solution to the api request https from the ip api

--- src/services/getIp.js
+++ src/services/getIp.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const getIp = async () => {
-    const URL = `http://api.ipstack.com/check?access_key=678a04a58d498c1c511493354ec6a87e`;
+    const URL = `https://api.ipstack.com/check?access_key=678a04a58d498c1c511493354ec6a87e`;
     const res = await axios(URL)
     return res
 }
