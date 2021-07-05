a78c4badb1a600c2846ca5a855b423e6b1f94cd0
sheyko <sheyko.wk@gmail.com>
2021-07-05 11:27:12 -0500

    change the api, ip to navigator geolocation

--- src/components/WeatherContainer.js
+++ src/components/WeatherContainer.js
@@ -1,5 +1,4 @@
 import { useState, useEffect } from 'react';
-import getIp from '../services/getIp';
 import getWeather from '../services/getWeather';
 import images from '../images.json';
 import Spinner from './Spinner'
@@ -23,33 +22,32 @@ const WeatherContainer = () => {
     const [wind, setWind] = useState(0);
     const [rain, setRain] = useState(0)
     const [img, setImg] = useState('');
-    const [hasDataIp, setHasDataIp] = useState(false);
+    const [hasDataLocation, setHasDataLocation] = useState(false);
     const [hasDataWeather, setHasDataWeather] = useState(false)
     //use of use state for the Weather api
 
     useEffect(() => {
-        getIp().then((res) =>{
-            setHasDataIp(true)
-            setState(res.data.region_name)
-            setCountry(res.data.country_name)
-            console.log(res.data)
-            getWeather(state).then((info) =>{
+        navigator.geolocation.getCurrentPosition((pos) =>{
+            setHasDataLocation(true)
+            getWeather(pos.coords.latitude, pos.coords.longitude).then((res) =>{
                 setHasDataWeather(true)
-                setTempCelcious(info.data.current.temp_c);
-                setTempFarenheit(info.data.current.temp_f);
-                setImg(images[info.data.current.condition.text].img);
-                setIcon(info.data.current.condition.icon);
-                setDescription(info.data.current.condition.text);
-                setCloudy(info.data.current.cloud);
-                setHumidity(info.data.current.humidity);
-                setWind(info.data.current.wind_kph);
-                setRain(info.data.current.precip_mm);
+                setState(res.data.location.region)
+                setCountry(res.data.location.country)
+                setTempCelcious(res.data.current.temp_c);
+                setTempFarenheit(res.data.current.temp_f);
+                setImg(images[res.data.current.condition.text].img);
+                setIcon(res.data.current.condition.icon);
+                setDescription(res.data.current.condition.text);
+                setCloudy(res.data.current.cloud);
+                setHumidity(res.data.current.humidity);
+                setWind(res.data.current.wind_kph);
+                setRain(res.data.current.precip_mm);
             })
         })
-    }, [state])
+    }, [])
     return(
         <div className='weather-container' style={{backgroundImage: `url(${img})`}}>
-            {hasDataIp && hasDataWeather ? (
+            {hasDataLocation && hasDataWeather ? (
                 <>
                 <Weather tempCelsious={tempCelsious} tempFarenheit={tempFarenheit} state={state} country={country} icon={icon} description={description}/>
                     <WeatherInfo cloudy={cloudy} humidity={humidity} wind={wind} rain={rain} />
--- src/services/getIp.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios'
-const getIp = async () => {
-    const URL = `https://api.ipstack.com/check?access_key=678a04a58d498c1c511493354ec6a87e`;
-    const res = await axios(URL)
-    return res
-}
-export default getIp
